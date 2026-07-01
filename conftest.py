19e902c30e5f1f81dfe319db0095a7e3d3e1b58d
LeemaJosephine <leemajosephine3@gmail.com>
2026-07-01 09:41:26 +0530

    Added env file

--- conftest.py
+++ conftest.py
@@ -1,12 +1,18 @@
+import os
+from dotenv import load_dotenv
 import pytest
 from playwright.sync_api import sync_playwright
+
+load_dotenv()
+
+url = os.getenv("URL")
 
 @pytest.fixture
 def page():  # name of the fixture
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=False)  # headless
         page = browser.new_page()
-        page.goto("http://demoblaze.com/index.html")
+        page.goto(url)
         page.set_default_timeout(25000)
         yield page
         browser.close()
