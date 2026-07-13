8252b0882f23eb84b09e34754e4cf7784458ae6e
LeemaJosephine <your.email@example.com>
2026-07-13 09:29:45 +0530

    Day-17

---
+++ tests/test_sigin.py
@@ -0,0 +1,11 @@
+import pytest
+import allure
+
+from pages.login_page import LoginPage
+
+@allure.title("Sample")
+@allure.description("To test the valid and invalid scenario")
+def test_login(page):
+
+    obj = LoginPage(page)  # class constructor , create the object for the class
+    obj.login("testuser","testuser")  # using the object we are calling the method
